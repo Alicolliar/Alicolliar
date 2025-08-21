29d577e346608f049622e423d03a460b7bf72f3a
Alicolliar <alicolliar@gmail.com>
2025-08-22 00:24:11 +0100

    refactor: BlogPostingApp uses tview instead of Bubbletea

--- blogPostingApp/main.go
+++ blogPostingApp/main.go
@@ -10,53 +10,27 @@ import (
 	"strings"
 	"time"
 
-	"github.com/charmbracelet/bubbles/textarea"
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-)
-
-const (
-	titleIn = iota
-	subIn
-	postIn
-	tagsIn
-	secretIn
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 var BASE_URL = os.Getenv("BLOG_BASEURL")
 
-var (
-	inputStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#fcd0b1"))
-)
-
-func postMessage(secret string, tagString string, post postModel) error {
+func postMessage(post postModel) {
 	var toPost bytes.Buffer
-	post.Tags = strings.Split(tagString, ",")
 	post.Timestamp = time.Now()
 	enc := json.NewEncoder(&toPost)
 	enc.Encode(post)
 	req, err := http.NewRequest(http.MethodPost, BASE_URL+"/blog/new", &toPost)
 	if err != nil {
-		return err
+		log.Panicln("HTTP NewReq err", err)
 	}
-	req.Header.Set("Authorization", "Bearer "+secret)
+	req.Header.Set("Authorization", "Bearer "+post.SecretKey)
 	req.Header.Set("content-type", "application/json")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil || resp.StatusCode != http.StatusAccepted {
-		return err
+		log.Panicf("Post Title: %v\nAuth Key: %v\n\nHTTP Send Err: %v", post.Title, post.SecretKey, err)
 	}
-	return nil
-}
-
-type commandModel struct {
-	Secret    string
-	Post      postModel
-	postIn    textarea.Model
-	tagString string
-	Inputs    []textinput.Model
-	focused   int
-	Err       string
 }
 
 type postModel struct {
@@ -65,101 +39,46 @@ type postModel struct {
 	Subtitle  string    `json:"subtitle"`
 	Tags      []string  `json:"tags"`
 	PostText  string    `json:"postText"`
+	SecretKey string    `json:"-"`
 }
 
-func initialModel() commandModel {
-	var newModel = commandModel{
-		Inputs: make([]textinput.Model, 5),
-	}
-	newModel.Inputs[titleIn] = textinput.New()
-	newModel.Inputs[titleIn].Focus()
-	newModel.Inputs[subIn] = textinput.New()
-	newModel.postIn = textarea.New()
-	newModel.postIn.SetHeight(25)
-	newModel.postIn.SetWidth(150)
-	newModel.Inputs[tagsIn] = textinput.New()
-	newModel.Inputs[secretIn] = textinput.New()
-	newModel.focused = 0
-	return newModel
+func (thePost postModel) splitTags(mashText string) {
+	thePost.Tags = strings.Split(mashText, ",")
 }
 
-func (model commandModel) Init() tea.Cmd {
-	return textinput.Blink
+func (thePost postModel) setPostTitle(title string) {
+	thePost.Title = title
 }
 
-func (model commandModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd = make([]tea.Cmd, len(model.Inputs)+1)
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyTab:
-			model.focused++
-			if model.focused > 4 {
-				model.focused = 0
-			}
-		case tea.KeyShiftTab:
-			model.focused--
-			if model.focused < 0 {
-				model.focused = 4
-			}
-		case tea.KeyCtrlC, tea.KeyEsc, tea.KeyCtrlQ:
-			return model, tea.Quit
-		case tea.KeyEnter:
-			if model.focused == secretIn {
-				if err := postMessage(model.Secret, model.tagString, model.Post); err != nil {
-					model.Err = err.Error()
-				}
-			}
-		}
-		model.postIn.Blur()
-		for i := range model.Inputs {
-			model.Inputs[i].Blur()
-		}
-		if model.focused == postIn {
-			model.postIn.Focus()
-		} else {
-			model.Inputs[model.focused].Focus()
-		}
-	}
-
-	model.Post.Title = model.Inputs[titleIn].Value()
-	model.Post.Subtitle = model.Inputs[subIn].Value()
-	model.Post.PostText = model.postIn.Value()
-	model.tagString = model.Inputs[tagsIn].Value()
-	model.Secret = model.Inputs[secretIn].Value()
-
-	for i := range model.Inputs {
-		model.Inputs[i], cmds[i] = model.Inputs[i].Update(msg)
-	}
-	model.postIn, cmds[4] = model.postIn.Update(msg)
-
-	return model, tea.Batch(cmds...)
+func (thePost postModel) setSubTitle(sub string) {
+	thePost.Subtitle = sub
 }
 
-func (model commandModel) View() string {
-	return fmt.Sprintf(
-		"%s\n%s %s\n%s %s\n%s %s\n%s \n%s\n\n%s %s\n%s %s\n%s",
-		inputStyle.Bold(true).Render("Alicolliar's Blog Posting TUI"),
-		inputStyle.Render("Posting to:"),
-		inputStyle.Render(BASE_URL),
-		inputStyle.Render("Post Title"),
-		model.Inputs[titleIn].View(),
-		inputStyle.Render("Post Subtitle"),
-		model.Inputs[subIn].View(),
-		inputStyle.Render("Post Text"),
-		model.postIn.View(),
-		inputStyle.Render("Post Tags (Comma-seperated)"),
-		model.Inputs[tagsIn].View(),
-		inputStyle.Render("Site Secret"),
-		model.Inputs[secretIn].View(),
-		inputStyle.Render("(Press Enter while selecting 'Site Secret' to Send to platform)"),
-	)
+func (thePost postModel) setPostText(text string) {
+	thePost.PostText = text
+}
+
+func (thePost postModel) setSecret(text string) {
+	thePost.SecretKey = text
 }
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+	if _, test := os.LookupEnv("BLOG_BASEURL"); !test {
+		fmt.Print("Set `BLOG_BASEURL` to ensure your posts will actually be sent somewhere.\n\n")
+		os.Exit(0)
+	}
+	theNewPost := postModel{}
+	app := tview.NewApplication()
+	form := tview.NewForm().
+		AddTextView("Posting to ", BASE_URL, 50, 0, false, false).
+		AddInputField("Title", "", 50, nil, func(text string) { theNewPost.Title = text }).
+		AddInputField("Subtitle", "", 50, nil, func(text string) { theNewPost.Subtitle = text }).
+		AddInputField("Tags", "", 50, nil, func(text string) { theNewPost.Tags = strings.Split(text, ",") }).
+		AddTextArea("Actual Post", "", 0, 25, 0, func(text string) { theNewPost.PostText = text }).
+		AddPasswordField("Secret Key", "", 50, '*', func(text string) { theNewPost.SecretKey = text }).
+		AddButton("Submit Post", func() { postMessage(theNewPost); app.Stop() })
+	form.SetBorder(true).SetBorderColor(tcell.NewRGBColor(252, 208, 177)).SetTitle("Alicolliar's Blog Posting App")
+	if err := app.SetRoot(form, true).EnablePaste(true).Run(); err != nil {
+		log.Panicln(err)
 	}
 }
